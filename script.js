cd09fbd073262953c218ae735fc376424fd223dc
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-13 16:13:12 +0200

    added refresh button

--- script.js
+++ script.js
@@ -11,8 +11,13 @@ createApp({
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((mail) => {
                     this.emailList.push(mail.data.response);
-                    console.log(this.emailList);
                 });
+        },
+        refreshMails() {
+            this.emailList = [];
+            for (let i = 0; i < 10; i++) {
+                this.generateMail();
+            }
         },
     },
     created() {
